57464e23cf04df99e9d01e2f80a0855131991590
alex_agh <aghajanyanalexander@gmail.com>
2022-09-11 18:15:31 +0400

    added bresenham algorithm

--- main.py
+++ main.py
@@ -1,36 +1,42 @@
-import pygame as pg
-from OpenGL.GL import *
+import pygame
+from bresenham import Bresenham
 
 
 class App:
     def __init__(self):
-        """ Initialise the program """
-        pg.init()
-
-        pg.display.set_mode((640, 480), pg.OPENGL | pg.DOUBLEBUF)
-        self.clock = pg.time.Clock()
-
-        glClearColor(0.1, 0.2, 0.2, 1)
+        self.background_colour = (229, 255, 255)
+        (width, height) = (800, 600)
+        self.screen = pygame.display.set_mode((width, height))
+        pygame.display.set_caption('Draw Line')
+        self.screen.fill(self.background_colour)
         self.main_loop()
 
     def main_loop(self):
         """ Run the app """
-
         running = True
+        count = 0
         while running:
-            for event in pg.event.get():
-                if event.type == pg.QUIT:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
                     running = False
-            glClear(GL_COLOR_BUFFER_BIT)
-            pg.display.flip()
-
-            self.clock.tick(60)
-        self.quit()
+                if event.type == pygame.MOUSEBUTTONUP:
+                    color = (24, 28, 28)
+                    if count == 0:
+                        pos1 = pygame.mouse.get_pos()
+                        count += 1
+                    else:
+                        pos2 = pygame.mouse.get_pos()
+                        bresenham = Bresenham(pos1[0], pos1[1], pos2[0], pos2[1])
+                        coordinates = bresenham.get_coordinates()
+                        for coordinate in coordinates:
+                            self.screen.set_at(coordinate, color)
+                        count = 0
+                pygame.display.flip()
 
     @staticmethod
     def quit():
         """ cleanup the app, run exit code """
-        pg.quit()
+        pygame.quit()
 
 
 if __name__ == "__main__":
