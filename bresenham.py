61e47ab10c9b30e7a0f04e6a5e65b11a5bdf0c81
alex_agh <aghajanyanalexander@gmail.com>
2022-09-20 18:53:08 +0400

    added polygon drawing method

--- bresenham.py
+++ bresenham.py
@@ -1,42 +1,37 @@
-class Bresenham:
-    def __init__(self, x1, y1, x2, y2):
-        self.x1 = x1
-        self.y1 = y1
-        self.x2 = x2
-        self.y2 = y2
+def get_coordinates(coord_01, coord_02):
+    x1, y1 = coord_01[0], coord_01[1]
+    x2, y2 = coord_02[0], coord_02[1]
 
-    def get_coordinates(self):
-        x1, y1 = self.x1, self.y1
-        x2, y2 = self.x2, self.y2
+    w = x2 - x1
+    h = y2 - y1
+    dx1, dy1 = 0, 0
+    dx2, dy2 = 0, 0
 
-        w = x2 - x1
-        h = y2 - y1
-        dx1, dy1 = 0, 0
-        dx2, dy2 = 0, 0
+    dx1 = -1 if w < 0 else 1
+    dy1 = -1 if h < 0 else 1
+    dx2 = -1 if w < 0 else 1
 
-        dx1 = -1 if w < 0 else 1
-        dy1 = -1 if h < 0 else 1
-        dx2 = -1 if w < 0 else 1
+    longest = abs(w)
+    shortest = abs(h)
 
-        longest = abs(w)
-        shortest = abs(h)
+    if not (longest > shortest):
+        longest = abs(h)
+        shortest = abs(w)
+        dy2 = -1 if h < 0 else 1
+        dx2 = 0
 
-        if not (longest > shortest):
-            longest = abs(h)
-            shortest = abs(w)
-            dy2 = -1 if h < 0 else 1
-            dx2 = 0
+    numerator = longest >> 1
+    coordinates = []
+    for i in range(0, longest):
+        coordinates.append((x1, y1))
+        numerator += shortest
+        if not (numerator < longest):
+            numerator -= longest
+            x1 += dx1
+            y1 += dy1
+        else:
+            x1 += dx2
+            y1 += dy2
+    return coordinates
 
-        numerator = longest >> 1
-        coordinates = []
-        for i in range(0, longest):
-            coordinates.append((x1, y1))
-            numerator += shortest
-            if not (numerator < longest):
-                numerator -= longest
-                x1 += dx1
-                y1 += dy1
-            else:
-                x1 += dx2
-                y1 += dy2
-        return coordinates
+
--- main.py
+++ main.py
@@ -1,36 +1,67 @@
 import pygame
-from bresenham import Bresenham
+from bresenham import get_coordinates
+from math import sin, cos, pi, floor, ceil
 
 
 class App:
     def __init__(self):
-        self.background_colour = (229, 255, 255)
+        self.background_colour = (47, 79, 79)
         (width, height) = (800, 600)
         self.screen = pygame.display.set_mode((width, height))
         pygame.display.set_caption('Draw Line')
         self.screen.fill(self.background_colour)
         self.main_loop()
 
+    @staticmethod
+    def get_polygon_coordinates(n, r=50, x_c=400, y_c=300):
+        coordinates = []
+        omega = pi / 2
+        for i in range(0, n):
+            x = x_c + r*cos(omega)
+            y = y_c + r*sin(omega)
+            omega += 2*pi / n
+            coordinates.append((ceil(x), ceil(y)))
+        coordinates.append(coordinates[0])
+        return coordinates
+
+    def draw_line(self, coordinates):
+        color = (124, 252, 0)
+        for coordinate in coordinates:
+            self.screen.set_at(coordinate, color)
+
     def main_loop(self):
         """ Run the app """
         running = True
         count = 0
+
+        pygame.display.flip()
+        n = input('Please enter number of vertices for the polygon:\n')
+        polygon = self.get_polygon_coordinates(int(n), r=150)
+
+        for i in range(0, len(polygon) - 1):
+            points = get_coordinates(polygon[i], polygon[i + 1])
+            self.draw_line(points)
+
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+
+                """
+                Drawing a line with mouse clicks.
+                """
+                """
                 if event.type == pygame.MOUSEBUTTONUP:
-                    color = (24, 28, 28)
                     if count == 0:
                         pos1 = pygame.mouse.get_pos()
                         count += 1
                     else:
                         pos2 = pygame.mouse.get_pos()
-                        bresenham = Bresenham(pos1[0], pos1[1], pos2[0], pos2[1])
-                        coordinates = bresenham.get_coordinates()
-                        for coordinate in coordinates:
-                            self.screen.set_at(coordinate, color)
+                        coordinates = get_coordinates(pos1, pos2)
+                        self.draw_line(coordinates)
                         count = 0
+                """
+
                 pygame.display.flip()
 
     @staticmethod
